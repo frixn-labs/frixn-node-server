d0342d49946f1b15d3deb7db0a1bcd9687733b63
manoj-loganathan <manojloganatham.dev@gmail.com>
2026-05-17 18:12:59 +0530

    Force IPv4 DNS resolution to fix Render ENETUNREACH IPv6 issue

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const dns = require('dns');
+dns.setDefaultResultOrder('ipv4first');
 const cors = require('cors');
 const nodemailer = require('nodemailer');
 require('dotenv').config();
